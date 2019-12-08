dae10dbf4104b2f89e8e1fddf4f65b09b5c5dc84
tsunematsu21 <tsunematsu21@users.noreply.github.com>
2019-12-08 22:19:01 +0900

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import { Configuration } from '@nuxt/types'
+
+const nuxtConfig: Configuration = {
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxtjs/eslint-module'
+  ],
+  build: {
+    hotMiddleware: {
+      client: {
+        overlay: false
+      }
+    }
+  }
+}
+
+module.exports = nuxtConfig
